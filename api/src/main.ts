d32f2f44f8f4e0051f5ad05fa15cb0f04c9dad03
Andres Contreras <andres@ejemplo.com>
2025-12-10 16:57:46 +0100

    Arreglo Prisma Vercel

--- api/src/main.ts
+++ api/src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  
+  // Habilitar CORS para que tu Web pueda hablar con la API
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+  await app.listen(3000);
 }
 bootstrap();
